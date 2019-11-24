09bf684d10ef830bce14c1c88a8db42039eb57af
Amakhin <amakhinruslan@gmail.com>
2019-11-24 12:10:35 +0200

    Finished with interface builder

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -20,9 +20,8 @@ class ViewController: UIViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
