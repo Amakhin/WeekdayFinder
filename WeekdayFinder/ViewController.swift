445fdeaeabbf06e4f5b9f8d1f6475898a94af9b8
Amakhin <amakhinruslan@gmail.com>
2019-11-24 12:47:03 +0200

    Working with code

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var dataTF: UITextField!
+    @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func findDay() {
+    @IBAction func findWeekDay() {
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        resultLabel.text = capitalizedWeekday
         
     }
     
