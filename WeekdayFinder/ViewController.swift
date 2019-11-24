83f8623b4663dc3452655d65c66dac1bebbe549d
Amakhin <amakhinruslan@gmail.com>
2019-11-24 03:43:18 +0200

    Initial Commit

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Amakhin Ruslan on 24.11.2019.
+//  Copyright © 2019 Amakhin Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
